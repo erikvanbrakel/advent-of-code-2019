0eea3c10aaf0a630558d3f694369279e5f12abc1
Erik van Brakel <erik.van.brakel@bynder.com>
2019-12-04 13:53:43 +0100

    Advent of code day 3

---
+++ pkg/pathfinder.go
@@ -0,0 +1,81 @@
+package pkg
+
+import (
+	"errors"
+	"strconv"
+)
+type Coordinate struct {
+	X, Y int
+}
+
+type Path struct {
+	Coordinates []Coordinate
+}
+
+type Pathfinder struct {
+	Path    Path
+	Current Coordinate
+}
+
+func (c *Coordinate) ManhattanDistance(o *Coordinate) int {
+	return absInt(c.X - o.X) + absInt(c.Y - o.Y)
+}
+
+func (p *Path) Intersect(p2 *Path) []Coordinate {
+	intersections := []Coordinate {}
+	for _,c1 := range p.Coordinates {
+		for _,c2 := range p2.Coordinates {
+			if c1.X == c2.X && c1.Y == c2.Y {
+				intersections = append(intersections, c1)
+			}
+		}
+	}
+	return intersections
+}
+
+func (p *Path) AddCoordinate(c Coordinate) {
+	p.Coordinates = append(p.Coordinates, c)
+}
+
+func absInt(i int) int {
+	if i < 0 { return -i } else { return i }
+}
+
+func (p *Pathfinder) Move(instructions ...string) error {
+	for _, instruction := range instructions {
+		direction := instruction[0]
+		amount, err := strconv.Atoi(instruction[1:])
+		if err != nil {
+			return err
+		}
+		switch direction {
+		case 'U':
+			for y := 0; y < amount; y++ {
+				p.Current.Y += 1
+				p.Path.AddCoordinate(p.Current)
+			}
+			break
+		case 'D':
+			for y := 0; y < amount; y++ {
+				p.Current.Y -= 1
+				p.Path.AddCoordinate(p.Current)
+			}
+			break
+		case 'L':
+			for x := 0; x < amount; x++ {
+				p.Current.X -= 1
+				p.Path.AddCoordinate(p.Current)
+			}
+			break
+		case 'R':
+			for x := 0; x < amount; x++ {
+				p.Current.X += 1
+				p.Path.AddCoordinate(p.Current)
+			}
+			break
+		default:
+			return errors.New("oops!")
+		}
+	}
+	return nil
+}
