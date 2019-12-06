98fc868ff7a8141276157804659e158b2aa8d4dd
Erik van Brakel <erik.van.brakel@bynder.com>
2019-12-06 14:53:46 +0100

    Advent of code day 5

--- pkg/computer.go
+++ pkg/computer.go
@@ -1,97 +1,135 @@
 package pkg
 
 import (
-    "bufio"
-    "bytes"
-    "os"
-    "strconv"
+	"bufio"
+	"bytes"
+	"os"
+	"strconv"
 )
 
 type Computer struct {
-    Memory []int
-    Cursor int
-    Halted bool
+	Memory             []int
+	InstructionPointer int
+	InputRegister      int
+	OutputRegister     int
+	Halted             bool
 }
 
-var opCodes = map[int]func(c *Computer){
-    1: func(c *Computer) {
-        addr1 := c.Memory[c.Cursor+1]
-        addr2 := c.Memory[c.Cursor+2]
-        addr3 := c.Memory[c.Cursor+3]
-
-        c.Memory[addr3] = c.Memory[addr1] + c.Memory[addr2]
-        c.Cursor += 4
-    },
-    2: func(c *Computer) {
-        addr1 := c.Memory[c.Cursor+1]
-        addr2 := c.Memory[c.Cursor+2]
-        addr3 := c.Memory[c.Cursor+3]
-
-        c.Memory[addr3] = c.Memory[addr1] * c.Memory[addr2]
-        c.Cursor += 4
-    },
-    99: func(c *Computer) {
-        c.Halted = true
-    },
+var opCodes = map[int]func(c *Computer, direct []bool) {
+	// add
+	1: func(c *Computer, direct []bool) {
+        result := c.GetMemoryValue(c.InstructionPointer+1, direct[0]) + c.GetMemoryValue(c.InstructionPointer+2, direct[1])
+        c.SetMemoryValue(c.InstructionPointer+3, result, direct[2])
+		c.InstructionPointer += 4
+	},
+	// multiply
+	2: func(c *Computer, direct []bool) {
+		result := c.GetMemoryValue(c.InstructionPointer+1, direct[0]) * c.GetMemoryValue(c.InstructionPointer+2, direct[1])
+		c.SetMemoryValue(c.InstructionPointer+3, result, direct[2])
+		c.InstructionPointer += 4
+	},
+	// read input
+	3: func(c *Computer, direct []bool) {
+		c.SetMemoryValue(c.InstructionPointer+1, c.InputRegister, direct[0])
+		c.InstructionPointer += 2
+	},
+	// write output
+	4: func(c *Computer, direct []bool) {
+		c.OutputRegister = c.GetMemoryValue(c.InstructionPointer+1, direct[0])
+		c.InstructionPointer += 2
+	},
+	// halt
+	99: func(c *Computer, direct []bool) {
+		c.Halted = true
+	},
 }
 
 func NewComputer(initialMemory []int) *Computer {
-    return &Computer{
-        Memory: initialMemory,
-        Cursor: 0,
-        Halted: false,
+	return &Computer{
+		Memory:             initialMemory,
+		InstructionPointer: 0,
+		Halted:             false,
+	}
+}
+
+func (c *Computer) GetMemoryValue(index int, direct bool) int {
+    if direct {
+        return c.Memory[index]
+    } else {
+        return c.GetMemoryValue(c.Memory[index] , true)
     }
 }
 
-func (c *Computer) NextInstruction() int {
-    opCode := c.Memory[c.Cursor]
-    return opCode
+func (c *Computer) SetMemoryValue(index, value int, direct bool) {
+    if direct {
+        c.Memory[index] = value
+    } else {
+        c.SetMemoryValue(c.Memory[index], value,true)
+    }
+}
+
+type Instruction struct {
+	OpCode int
+	addressModes []bool
+}
+
+func (c *Computer) NextInstruction() Instruction {
+	opCode := c.GetMemoryValue(c.InstructionPointer, true)
+
+	return Instruction {
+		opCode % 100,
+		[]bool {
+			(opCode / 100) % 10 == 1,
+			(opCode / 1000) % 10 == 1,
+			(opCode / 10000) % 10 == 1,
+		},
+	}
 }
 
 func (c *Computer) Run() {
-    for {
-        if c.Halted {
-            return
-        }
-        opCode := c.NextInstruction()
-        opCodes[opCode](c)
-    }
+	for {
+		if c.Halted {
+			return
+		}
+		instruction := c.NextInstruction()
+		opCodes[instruction.OpCode](c, instruction.addressModes)
+	}
 }
 
 func ComputerFromFile(path string) (*Computer, error) {
-    file, err := os.Open(path)
-    defer file.Close()
+	file, err := os.Open(path)
+	defer file.Close()
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    initialMemory := []int{}
-    scanner := bufio.NewScanner(file)
-    scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	initialMemory := []int{}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 
-        search := []byte(",")
-        if i := bytes.Index(data, search); i >= 0 {
-            return i + len(search), data[0:i], nil
-        }
+		search := []byte(",")
+		if i := bytes.Index(data, search); i >= 0 {
+			return i + len(search), data[0:i], nil
+		}
 
-        if atEOF && len(data) != 0 {
-            return len(data), data, nil
-        }
+		if atEOF && len(data) != 0 {
+			return len(data), data, nil
+		}
 
-        return 0, nil, nil
-    })
-    for scanner.Scan() {
-        memory, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-            return nil, err
-        }
-        initialMemory = append(initialMemory, memory)
-    }
+		return 0, nil, nil
+	})
+	for scanner.Scan() {
+		memory, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		initialMemory = append(initialMemory, memory)
+	}
 
-    return &Computer{
-        Memory: initialMemory,
-        Cursor: 0,
-        Halted: false,
-    }, nil
+	return &Computer{
+		Memory:             initialMemory,
+		InstructionPointer: 0,
+		Halted:             false,
+	}, nil
 }
--- pkg/computer_test.go
+++ pkg/computer_test.go
@@ -1,68 +1,128 @@
 package pkg
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type ResultSet struct {
-    Input  []int
-    Output []int
+	Program   []int
+	Input int
+	Output int
+	Assertion func(c *Computer, t *testing.T)
+}
+
+func AssertOutput(value int) func(c *Computer, t *testing.T) {
+    return func(c *Computer, t *testing.T) {
+        if c.OutputRegister != value {
+            t.Errorf("output register should contain %v, found %v", value, c.OutputRegister)
+        }
+    }
+}
+
+func AssertMemoryValue(index, value int) func(c *Computer, t *testing.T) {
+	return func(c *Computer, t *testing.T) {
+		if c.Memory[index] != value {
+			t.Errorf("memory at %v should be %v, found %v", index, value, c.Memory[index])
+		}
+	}
+}
+
+func Aggregate(assertions ...func(c *Computer, t *testing.T)) func(c *Computer, t *testing.T) {
+	return func(c *Computer, t *testing.T) {
+		for _, a := range assertions {
+			a(c, t)
+		}
+	}
 }
 
 func TestComputer(t *testing.T) {
-    var expectedResults = []ResultSet{
+	var expectedResults = []ResultSet{
+		{
+			Program:   []int{1, 0, 0, 0, 99},
+			Assertion: AssertMemoryValue(0, 2),
+		},
+		{
+			Program:   []int{2, 3, 0, 3, 99},
+			Assertion: AssertMemoryValue(3, 6),
+		},
+		{
+			Program:   []int{2, 4, 4, 5, 99, 0},
+			Assertion: AssertMemoryValue(5, 9801),
+		},
+		{
+			Program: []int{1, 1, 1, 4, 99, 5, 6, 0, 99},
+			Assertion: Aggregate(
+				AssertMemoryValue(0, 30),
+				AssertMemoryValue(5, 5),
+			),
+		},
         {
-            Input:  []int{1, 0, 0, 0, 99},
-            Output: []int{2, 0, 0, 0, 99},
+            Program: []int {3, 0, 4, 0, 99},
+            Input: 123,
+            Output: 444,
+            Assertion: AssertOutput(123),
         },
         {
-            Input:  []int{2, 3, 0, 3, 99},
-            Output: []int{2, 3, 0, 6, 99},
+            Program: []int {1003, 0, 1004, 0, 99},
+            Input: 123,
+            Output: 444,
+            Assertion: AssertOutput(123),
         },
         {
-            Input:  []int{2, 4, 4, 5, 99, 0},
-            Output: []int{2, 4, 4, 5, 99, 9801},
+            Program: []int {1002,4,3,4,33},
+            Assertion: AssertMemoryValue(4, 99),
         },
-        {
-            Input:  []int{1, 1, 1, 4, 99, 5, 6, 0, 99},
-            Output: []int{30, 1, 1, 4, 2, 5, 6, 0, 99},
-        },
-    }
+	}
 
-    for _, v := range expectedResults {
-        computer := NewComputer(v.Input)
-        computer.Run()
-        for i, a := range computer.Memory {
-            if v.Output[i] != a {
-                t.Error("memory doesn't match")
-            }
-        }
-    }
+	for i, v := range expectedResults {
+		t.Run(fmt.Sprintf("test-%v", i), func(t *testing.T) {
+			computer := NewComputer(v.Program)
+			computer.InputRegister = v.Input
+			computer.OutputRegister = v.Output
+			computer.Run()
+			v.Assertion(computer, t)
+		})
+	}
+	{
+		computer, err := ComputerFromFile("../input_files/day_2.txt")
+		if err != nil {
+			t.Error(err)
+		} else {
+			computer.Memory[1] = 12
+			computer.Memory[2] = 2
+			computer.Run()
+			t.Logf("Address #0 contains %v", computer.Memory[0])
+		}
+	}
+	{
+		computer, err := ComputerFromFile("../input_files/day_5.txt")
+		if err != nil {
+			t.Error(err)
+		} else {
+			computer.InputRegister = 1
+			computer.Run()
+			t.Logf("Diagnostics ran successfully! Output: %v", computer.OutputRegister)
+		}
+	}
 
-    computer, err := ComputerFromFile("../input_files/day_2.txt")
-    if err != nil {
-        t.Error(err)
-    } else {
-        computer.Memory[1] = 12
-        computer.Memory[2] = 2
-        computer.Run()
-        t.Logf("Address #0 contains %v", computer.Memory[0])
-    }
 }
 
 func TestNounAndVerb(t *testing.T) {
-    expectedValue := 19690720
-    for k := 0; k < 100; k++ {
-        for v := 0; v < 100; v++ {
-            computer, err := ComputerFromFile("../input_files/day_2.txt")
-            if err != nil {
-                t.Error(err)
-            } else {
-                computer.Memory[1] = k
-                computer.Memory[2] = v
-                computer.Run()
-                if computer.Memory[0] == expectedValue {
-                    t.Logf("Noun: %v, Verb: %v, result: %v", computer.Memory[1], computer.Memory[2], 100*computer.Memory[1]+computer.Memory[2])
-                }
-            }
-        }
-    }
+	expectedValue := 19690720
+	for k := 0; k < 100; k++ {
+		for v := 0; v < 100; v++ {
+			computer, err := ComputerFromFile("../input_files/day_2.txt")
+			if err != nil {
+				t.Error(err)
+			} else {
+				computer.Memory[1] = k
+				computer.Memory[2] = v
+				computer.Run()
+				if computer.Memory[0] == expectedValue {
+					t.Logf("Noun: %v, Verb: %v, result: %v", computer.Memory[1], computer.Memory[2], 100*computer.Memory[1]+computer.Memory[2])
+				}
+			}
+		}
+	}
 }
